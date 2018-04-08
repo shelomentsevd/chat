b26a25466f2d7c9b8ff154fc8f578de0c8b702d9
Shelomentsev Dmitriy <shelomentsev@protonmail.com>
2018-04-08 16:28:10 +0300

    :shirt: typo

--- src/handlers/registration/registration.go
+++ src/handlers/registration/registration.go
@@ -12,28 +12,28 @@ import (
 	"github.com/labstack/gommon/log"
 )
 
-func RegisterUser(c echo.Context) error {
+func RegisterUser(ctx echo.Context) error {
 	var user models.User
-	if err := c.Bind(&user); err != nil {
+	if err := ctx.Bind(&user); err != nil {
 		log.Errorf("parse error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
+		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
-	if err := c.Validate(&user); err != nil {
+	if err := ctx.Validate(&user); err != nil {
 		log.Infof("validation error: %v", err)
-		return c.NoContent(http.StatusBadRequest)
+		return ctx.NoContent(http.StatusBadRequest)
 	}
 
 	if err := users.CreateUser(&user); err != nil {
 		log.Infof("create user error: %v", err)
-		return c.NoContent(http.StatusBadRequest)
+		return ctx.NoContent(http.StatusBadRequest)
 	}
 
 	out := bytes.NewBuffer(nil)
 	if err := jsonapi.MarshalPayload(out, &user); err != nil {
 		log.Errorf("marshal error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
+		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.Blob(http.StatusCreated, jsonapi.MediaType, out.Bytes())
+	return ctx.Blob(http.StatusCreated, jsonapi.MediaType, out.Bytes())
 }
