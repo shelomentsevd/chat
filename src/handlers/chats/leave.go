90f442911dddbd8c595d8509f88920e3a0588e20
Shelomentsev Dmitriy <shelomentsev@protonmail.com>
2018-04-14 22:13:36 +0300

    :star: POST /chats/ID/leave

--- src/handlers/chats/leave.go
+++ src/handlers/chats/leave.go
@@ -1,7 +1,43 @@
 package chats
 
-import "github.com/labstack/echo"
+import (
+	"db"
+	"handlers"
+
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+)
 
 func Leave(ctx echo.Context) error {
-	return nil
+	user, ok := ctx.Get(handlers.CurrentUserKey).(db.User)
+	if !ok {
+		log.Info("can't add non-authorized user to chat")
+		return ctx.NoContent(http.StatusUnauthorized)
+	}
+
+	chatStr := ctx.Param("chat")
+	chatID, err := strconv.ParseUint(chatStr, 10, 32)
+	if err != nil {
+		log.Infof("parse error: %v", err)
+		return ctx.NoContent(http.StatusBadRequest)
+	}
+
+	err = db.Delete(&db.Member{
+		UserID: user.ID,
+		ChatID: uint(chatID),
+	})
+	switch err {
+	case db.ErrPgForeignKeyViolation:
+		log.Infof("parse error: %v", err)
+		return ctx.NoContent(http.StatusBadRequest)
+	case db.ErrPgUniqueViolation:
+	default:
+		log.Errorf("database error: %v", err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+
+	return ctx.NoContent(http.StatusOK)
 }
